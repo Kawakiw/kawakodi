48a2d74d57d9ee89e302a56c76c93cc4a8f03b6f
Kawakiw <marsli.y@hotmail.fr>
2015-10-19 00:30:11 +0100

    Divertissement

--- plugin.video.divertissement/default.py
+++ plugin.video.divertissement/default.py
@@ -21,7 +21,11 @@ YOUTUBE_CHANNEL_IDA = "JustForLaughsTV"
 YOUTUBE_CHANNEL_IDB = "comediatvtube"
 YOUTUBE_CHANNEL_IDC = "failarmy"
 YOUTUBE_CHANNEL_IDD = "JukinVideoDotCom"
-
+YOUTUBE_CHANNEL_IDE = "UCsN_Ml220puJo9RFBHriAaw"
+YOUTUBE_CHANNEL_IDF = "MOMOBOUSFIHA"
+YOUTUBE_CHANNEL_IDG = "TheBlackmoussiba"
+YOUTUBE_CHANNEL_IDH = "MonsieurDream"
+YOUTUBE_CHANNEL_IDI = "NormalFaitDesVideos"
 
 
 
@@ -45,31 +49,66 @@ def main_list(params):
     plugintools.log("divertissement.main_list "+repr(params))
 
     plugintools.add_item( 
-        #action="", 
+        fanart='https://raw.githubusercontent.com/Kawakiw/kawakodi/master/ressources/fanarts/jfl.jpg', 
         title="Juste Pour Rire",
         url="plugin://plugin.video.youtube/user/"+YOUTUBE_CHANNEL_IDA+"/",
-        thumbnail=icon,
+        thumbnail='https://raw.githubusercontent.com/Kawakiw/kawakodi/master/ressources/thumbnails/jfl.png',
         folder=True )
 		
     plugintools.add_item( 
-        #action="", 
+        fanart='https://raw.githubusercontent.com/Kawakiw/kawakodi/master/ressources/fanarts/failarmy.jpg', 
         title="Comedia Al Oula",
         url="plugin://plugin.video.youtube/user/"+YOUTUBE_CHANNEL_IDB+"/",
-        thumbnail=icon,
+        thumbnail='https://raw.githubusercontent.com/Kawakiw/kawakodi/master/ressources/thumbnails/comedia.png',
         folder=True )
 		
     plugintools.add_item( 
-        #action="", 
+        fanart='https://raw.githubusercontent.com/Kawakiw/kawakodi/master/ressources/fanarts/failarmy.jpg', 
         title="FailArmy",
         url="plugin://plugin.video.youtube/user/"+YOUTUBE_CHANNEL_IDC+"/",
-        thumbnail=icon,
+        thumbnail='https://raw.githubusercontent.com/Kawakiw/kawakodi/master/ressources/thumbnails/failarmy.png',
         folder=True )
 		
     plugintools.add_item( 
-        #action="", 
+        fanart='https://raw.githubusercontent.com/Kawakiw/kawakodi/master/ressources/fanarts/jukinvideo.jpg', 
         title="JukinVideo",
         url="plugin://plugin.video.youtube/user/"+YOUTUBE_CHANNEL_IDD+"/",
-        thumbnail=icon,
+        thumbnail='https://raw.githubusercontent.com/Kawakiw/kawakodi/master/ressources/thumbnails/failarmy.png',
         folder=True )
+	
+    plugintools.add_item( 
+        fanart='https://raw.githubusercontent.com/Kawakiw/kawakodi/master/ressources/fanarts/darraj.jpg', 
+        title="Darraj",
+        url="plugin://plugin.video.youtube/channel/"+YOUTUBE_CHANNEL_IDE+"/",
+        thumbnail='https://raw.githubusercontent.com/Kawakiw/kawakodi/master/ressources/thumbnails/darraj.png',
+        folder=True )
+		
+    plugintools.add_item( 
+        fanart='https://raw.githubusercontent.com/Kawakiw/kawakodi/master/ressources/fanarts/momo.jpg', 
+        title="Momo - Hitradio",
+        url="plugin://plugin.video.youtube/user/"+YOUTUBE_CHANNEL_IDF+"/",
+        thumbnail='https://raw.githubusercontent.com/Kawakiw/kawakodi/master/ressources/thumbnails/momo.png',
+        folder=True )
+		
+    plugintools.add_item( 
+        fanart='https://raw.githubusercontent.com/Kawakiw/kawakodi/master/ressources/fanarts/blackmossiba.jpg', 
+        title="Black Mossiba",
+        url="plugin://plugin.video.youtube/user/"+YOUTUBE_CHANNEL_IDG+"/",
+        thumbnail='https://raw.githubusercontent.com/Kawakiw/kawakodi/master/ressources/thumbnails/blackmossiba.png',
+        folder=True )
+		
+    plugintools.add_item( 
+        fanart='https://raw.githubusercontent.com/Kawakiw/kawakodi/master/ressources/fanarts/cyprien.jpg', 
+        title="Cyprien",
+        url="plugin://plugin.video.youtube/user/"+YOUTUBE_CHANNEL_IDH+"/",
+        thumbnail='https://raw.githubusercontent.com/Kawakiw/kawakodi/master/ressources/thumbnails/cyprien.png',
+        folder=True )
+	
+    plugintools.add_item( 
+        fanart='https://raw.githubusercontent.com/Kawakiw/kawakodi/master/ressources/fanarts/norman.jpg', 
+        title="Normal Fait Des Vidéos",
+        url="plugin://plugin.video.youtube/user/"+YOUTUBE_CHANNEL_IDI+"/",
+        thumbnail='https://raw.githubusercontent.com/Kawakiw/kawakodi/master/ressources/thumbnails/norman.png',
+        folder=True )	
 
 run()
